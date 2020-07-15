d9e291c2dfc07a25bb1cc3d5d2a4b77af3e179d2
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-07-15 15:33:33 -0300

    test for session.save and session.persist

--- hibernate-demo/src/main/java/interview/hibernate/controllers/EmployeeController2.java
+++ hibernate-demo/src/main/java/interview/hibernate/controllers/EmployeeController2.java
@@ -3,17 +3,19 @@ package interview.hibernate.controllers;
 import interview.hibernate.daos.EmployeeDao;
 import interview.hibernate.exception.NoEmployeeException;
 import interview.hibernate.models.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import javax.xml.bind.PropertyException;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -27,6 +29,31 @@ public class EmployeeController2 {
     @GetMapping(value="/getEmployees")
     public List<Employee> getEmployees(){
         return employeeDao.get();
+    }
+
+    @Autowired
+    private EntityManager entityManager;
+    @GetMapping(value="/testSaveVsPersist")
+    public String testSaveVsPersist(@RequestParam("method") String method){
+        Employee em =null;
+        Session session = entityManager.unwrap(Session.class);
+        Employee employeeInTransaction1 = new Employee("TestName1","male","QA", LocalDate.of(2010,1,31));
+        Employee employeeInTransaction2 = new Employee("TestName2","male","QA", LocalDate.of(2010,1,31));
+
+        if(method.equals("save")){
+            Transaction tx = session.beginTransaction();
+            session.save(employeeInTransaction1); // can effect in transaction
+            tx.commit(); // transaction close
+            session.save(employeeInTransaction2); // can effect outside transaction aslo
+        }else if(method.equals("persist")){
+            Transaction tx = session.beginTransaction();
+            session.persist(employeeInTransaction1);  // can only effect inside transaction
+            tx.commit(); // transaction close
+            session.persist(employeeInTransaction2); // can NOT effect outside transaction
+
+        }
+
+        return String.valueOf(employeeInTransaction1.getId()) + "|" +String.valueOf(employeeInTransaction2.getId());
     }
 
 }
